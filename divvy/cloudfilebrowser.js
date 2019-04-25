e86e5a48796ce49edfa73a3128c1a839406d0c9a
Genesis Dwight Bentulan <genesisbentulan@gmail.com>
2019-04-25 16:49:36 +0800

    improve search box style

--- divvy/cloudfilebrowser.js
+++ divvy/cloudfilebrowser.js
@@ -991,7 +991,7 @@ var cloudFileBrowser = (function() {
                 cloudFileBrowser.selectedFiles[element] = new Array();
 
                 tableHTML += '<div class="search-wrapper">' +
-                                '<button id="js-search-open"><i class="fa fa-search fa-flip-horizontal search-open" aria-hidden="true"></i></button>' +
+                                '<button id="js-search-open" class="search-open"><i class="fa fa-search fa-flip-horizontal" aria-hidden="true"></i></button>' +
                                 '<span class="search-box-form-wrapper" id="js-search-box-wrapper">' +
                                 '<input type="text" id="js-search-box" class="search-box" placeholder="Search..."/>' +
                                 '<i class="fa fa-times-circle search-close search-close" id="js-search-close" aria-hidden="true"></i>' +
